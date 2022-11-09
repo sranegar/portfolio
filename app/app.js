adb79ef2c9dd23bd804028a7b2daa784b6c4c36e
Stephanie Ranegar <83993810+sranegar@users.noreply.github.com>
2022-11-09 13:38:51 -0500

    Add all projects and view details in modal

--- app/app.js
+++ app/app.js
@@ -1,21 +1,20 @@
 import * as MODEL from "./model.js";
 
  
-
 function route() {
   let hashTag = window.location.hash;
   let pageID = hashTag.replace("#", "");
  let pageIDArray = pageID.split("/");
-
+ 
  pageID = pageIDArray[0];
  let subPageID = pageIDArray[1];
 
   if (!pageID) {
-    MODEL.changePage("work");
+    MODEL.changePage("home");
   } else if (pageID == "" || pageID == "home") {
     MODEL.changePage(pageID);
   } else if (pageID == "work") {
-    MODEL.changePage(pageID);
+    MODEL.changePage(pageID, subPageID, openModal );
   } else {
     if (pageID === subPageID) {
       MODEL.changePage(pageID);
@@ -34,8 +33,25 @@ function route() {
      
     })
   }
+
+  function openModal() {
+     $(".work-btn-wrapper button").click(function (e) {
+      let projID = e.currentTarget.id;
+
+    
+      MODEL.viewProject(projID)
+      $("#modal").css("display",  "flex");
+     })
+  }
     
  
+
+  // function changePhoto() {}
+  // $("#modal .fa-circle-chevron-right").click(function () {
+  //   $("#modal").css("display", "none");
+  // })
+
+  
 function initApp() {
   $(window).on("hashchange", route);
 }
@@ -50,4 +66,6 @@ function initListeners() {
 $(document).ready(function () {
   initListeners();
   initApp();
+ 
+ 
 });
--- app/model.js
+++ app/model.js
@@ -1,38 +1,156 @@
+var columns = [
+  {
+    projects: [
+      {
+        id: 0,
+        title: "Irving Materials Inc.",
+        subtitle: "Next.js",
+        img: "assets/images/poster-truck.jpg",
+     subDetails: "Landing Page Solution",
+     details: "",
+     href: "https://driveimi.com/"
+      },
+      {
+        id: 1,
+        title: "GamePlanet E-Commerice",
+        subtitle: "PHP, MySQL, JavaScript",
+        img: "assets/images/game-planet2.jpg",
+        subDetails: "E-Commerce Web Solution",
+        details: "",
+        href: ""
+      },
+    ],
+  },
+  {
+    projects: [
+      {
+        id: 2,
+        title: "NewsLetter Template",
+        subtitle: "HTML, CSS, Mailchimp",
+        img: "assets/images/mas1.jpg",
+        subDetails: "Email Template Design",
+        details: "",
+        href: ""
+      },
+
+      {
+        id: 3,
+        title: "Brighter Days CBD",
+        subtitle: "Next.js",
+        img: "assets/images/brighter-days.jpg",
+        subDetails: "E-Commerce Design Solution",
+        details: "",
+        href: ""
+      },
+    ],
+  },
+  {
+    projects: [
+      {
+        id: 4,
+        title: "Music Player Mobile App",
+        subtitle: "REST API / React.js",
+        img: "assets/images/music-app.jpg",
+        subDetails: "MP3 Player",
+        details: "",
+        href: ""
+      },
+      {
+        id: 5,
+        title: "Website Design Overhaul",
+        subtitle: "WordPress",
+        img: "assets/images/hct.jpg",
+        subDetails: "Re-design Solution",
+        details: "",
+        href: ""
+      },
+    ],
+  },
+];
+ 
+var curProject = {};
+
 export function changePage(pageID, subPage, callback) {
   //HOME
   if (pageID == "" || pageID == "home") {
     $.get(`pages/${pageID}/home.html`, function (data) {
-      // $("#open").html(data);
-      $("html, body").animate({
-        scrollTop: 0
-      }, 200)
-      
+      // $("html, body").animate({
+      //   scrollTop: 0
+      // }, 200)
+      $("#app").html(data);
+      $("#nav-bar").css("top", "");
+      $("#nav-bar").css("bottom", "0");
     });
- 
-  // } else if (pageID == "work") {
-  //   $.get(`pages/${pageID}/${pageID}.html`, function (data) {
-  //     $("#app").html(data);
-  //    $("#nav-bar").css("display", "flex");
-           
-  //   });
+  } else if (pageID == "work") {
+    $.get(`pages/${pageID}/${pageID}.html`, function (data) {
+      $("#app").html(data);
+      $("#nav-bar").css("bottom", "");
+      $("#nav-bar").css("top", "0");
 
-  // } else if (pageID == "about") {
-  //   $.get(`pages/${pageID}/${pageID}.html`, function (data) {
-  //     $("#app").html(data);
-  //    $("#nav-bar").css("display", "flex");
-             
-  //   });
-     
-         
+      $("#modal .fa-xmark").click(function () {
+        $("#modal").css("display", "none");
+      });
+      let pageColumn = "";
+      
+      $.each(columns, (idx, col) => {
+        pageColumn += `<div class="work-col">`;
        
+        
+        $.each(col.projects, (id, proj) => {
+        
+          pageColumn += ` <div class="project-container" id=${proj.id}">
+          <div class="image-container proj1">     
+                  <img src=${proj.img} alt="Drive imi Project" />   
+              <div class="title-wrapper">
+                  <div class="title">
+                  ${proj.title}
+                  </div>
+                  <p class="subtitle">${proj.subtitle}</p>
+                  <div class="work-btn-wrapper">
+                      <button id=${idx}${id}${proj.id} type="button" class="work-btn">View More</button>
+                  </div>
+              </div>
+          </div>
+  </div>`;
+         
+        });
+      
+        pageColumn += `</div>`;
+      });
+      
+      $(".work-wrapper").append(pageColumn);
+      callback();
+     
+    });
+  } else if (pageID == "about") {
+    $.get(`pages/${pageID}/${pageID}.html`, function (data) {
+      $("#nav-bar").css("bottom", "");
+      $("#nav-bar").css("top", "0");
+      $("#app").html(data);
+    });
   } else {
     $.get(`pages/${pageID}/${pageID}.html`, function (data) {
       $("#app").html(data);
       $("#nav-bar").css("display", "flex");
-      
     });
   }
 }
 
+export function viewProject(projId) {
+  console.log(projId)
+  let projIdArray = projId.split("");
+  let colId = projIdArray[0]
+  let projIndex = projIdArray[1]
+  let projID = projIdArray[2]
  
+   curProject = columns[colId].projects[projIndex];
+   console.log(curProject)
+  if (curProject.id == projID) {
+    $("#modal-content img").attr("src", curProject.img)
+    $("#modal-content a").attr("href", curProject.href)
+    $("#modal-content h2").text(curProject.title)
+    $("#modal-content h4").text(curProject.subDetails)
+    $("#modal-content p").text(curProject.details);
+  }
  
+}
